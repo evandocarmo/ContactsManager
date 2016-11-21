f0d9e9629eba78bd4939c3e29b78d87390c3ac87
Elvis Oliveira <elvis.olv@gmail.com>
2016-11-21 10:15:40 -0200

    Upgraded description on database tables via comment on the files. Need to be improved.

--- assets/database/20161116.sql
+++ assets/database/20161116.sql
@@ -1,1 +1,8 @@
-ALTER TABLE `log` CHANGE `log_type` `log_type` ENUM('foreigner','user','visits','status','category','personal_users') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
+# Log 
+# ENUM specific definition for each error type.
+
+        ALTER TABLE `log` 
+              CHANGE `log_type` `log_type` 
+              ENUM('foreigner','user','visits','status','category','personal_users') 
+CHARACTER SET latin1 
+      COLLATE latin1_swedish_ci NOT NULL;
